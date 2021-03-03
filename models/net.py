233e11a55483ffa6a3b45427c373411f200f13e9
Caudiu Decean <Claudiu.Decean@porsche-engineering.ro>
2021-03-03 18:39:58 +0200

    Reached 93% accuracy on test dataset

--- models/net.py
+++ models/net.py
@@ -7,10 +7,14 @@ import numpy as np
 class Net(nn.Module):
     def __init__(self, num_classes=10):
         super(Net, self).__init__()
-        self.net = torchvision.models.resnet18(pretrained=True)
+        self.net = torchvision.models.resnet18(pretrained=False)
         num_ftrs = self.net.fc.in_features
         self.net.fc = nn.Linear(num_ftrs, num_classes)
-
+        # trick from https://gist.github.com/y0ast/d91d09565462125a1eb75acc65da1469
+        self.net.conv1 = nn.Conv2d(
+            3, 64, kernel_size=3, stride=1, padding=1, bias=False
+        )
+        self.net.maxpool = nn.Identity()
     def forward(self, x):
         x = self.net(x)
         return x
--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,35 @@
 import torch
 
+def train(net, trainloader, optimizer, loss_function, scheduler, device, epochs=50, testloader=None):
+    for epoch in range(epochs):  # loop over the dataset multiple times
+        train_loss = train_single_epoch(net, trainloader, optimizer, loss_function, device, epochs=50)
+        scheduler.step()
+        print(f'epoch: {epoch + 1}, training loss: {train_loss}')
+        if testloader is not None:
+            test_accuracy = test(net, testloader, device)
+            print(f'test accuracy {test_accuracy}')
+    print('Finished Training')
+
+def train_single_epoch(net, trainloader, optimizer, loss_function, device, epochs=50):
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
+        
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = loss_function(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        # print statistics
+        running_loss += loss.item()
+    train_loss = running_loss / len(trainloader)
+    return train_loss
+    
 def test(net, testloader, device):
     correct = 0
     total = 0
